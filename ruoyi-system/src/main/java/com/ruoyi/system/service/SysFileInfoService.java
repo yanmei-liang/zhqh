c58234a60f0f146c3f8eac5fd61df982c8470c38
yanmei-liang <yanmeiliang@foxmail.com>
2024-10-16 15:49:15 +0800

    添加文件信息表，修改通用上传方法

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/SysFileInfoService.java
@@ -0,0 +1,13 @@
+package com.ruoyi.system.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.ruoyi.system.domain.SysFileInfo;
+
+/**
+ * @author liangyanmei
+ * @Title: 文件信息表
+ * @Description:
+ * @date 2024/10/1614:47
+ */
+public interface SysFileInfoService extends IService<SysFileInfo> {
+}
