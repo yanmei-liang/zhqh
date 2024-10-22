3e4aef8e3229cdc2f8898a74cadf4d955c90190a
xioajie-chen <2507661758@qq.com>
2024-10-22 11:52:12 +0800

    行政区划页面接口

--- ruoyi-ui/src/api/government/DIVISION.js
+++ ruoyi-ui/src/api/government/DIVISION.js
@@ -1,10 +1,10 @@
 import request from '@/utils/request'
 // 获取用户列表
-export function listDIVISION(data) {
+export function listDIVISION(query) {
     return request({
         url: '/government/DIVISIONS/list',
         method: 'get',
-        data: data
+        params: query
     })
 }
 
@@ -13,15 +13,38 @@ export function mapStatistics(data) {
     return request({
         url: "",
         method: 'post',
-        data: data
+        data
     })
 }
 
 // 导出级别类型
 export function levelTypes(data) {
     return request({
-        url: '/government​/DIVISIONS​/exportStatisticallevelList',
+        url: '/government/DIVISIONS/exportStatisticallevelList',
         method: "post",
-        data: data
+        data
+    })
+}
+
+// 删除行政区划
+export function delDIVISION(divisionID) {
+    return request({
+        url: '/government/DIVISIONS/remove/' + divisionID,
+        method: 'delete'
+    })
+}
+// 统计级别  /government/DIVISIONS/selAdministrativeLevel/{administrativeDivisionCode}
+export function selLevel(DivisionCode) {
+    return request({
+        url: '/government/DIVISIONS/selAdministrativeLevel/' + { administrativeDivisionCode },
+        method: 'get'
+    })
+}
+
+// 统计面积
+export function selArea(administrativeDivisionCode) {
+    return request({
+        url: '/government/DIVISIONS/selAreaRadius/' + { administrativeDivisionCode },
+        method: 'get'
     })
 }
